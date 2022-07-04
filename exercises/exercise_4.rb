1690f59fb75dd9c1b239c940118517419b7340c0
mbbaltaji <baltaji@uwindsor.ca>
2022-07-04 15:07:13 -0400

    find stores using where AR method

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,35 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Surrey = Store.create(
+  name: 'Surrey', 
+  annual_revenue: 224000, 
+  mens_apparel: false, 
+  womens_apparel: true
+)
+
+Whislet = Store.create(
+  name: 'Whistler',
+  annual_revenue: 19000000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+Yaletown = Store.create(
+  name: 'Yaletown',
+  annual_revenue: 4300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "name: #{store.name}, Annual Revienue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel= true and annual_revenue < 1000000")
+
+@womens_stores.each do |store|
+  puts "name: #{store.name}, Annual Revienue: #{store.annual_revenue}"
+end
